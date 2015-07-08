525905c5056676514a9c16fd0f889c302ad7f423
wregen <W.Regenczuk@gmail.com>
2015-07-08 07:46:51 +0200

    votingSingle added; console output

--- app.js
+++ app.js
@@ -4,7 +4,68 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-var baseUrl = 'http://www.sejm.gov.pl/Sejm7.nsf/';
+var baseUrl = 'http://www.sejm.gov.pl/';
+
+function votingSingleDetailsMarszalek(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, html) {
+            if (error) {
+                return reject(error);
+            }
+            var $ = cheerio.load(html);
+            var out = [];
+            // second table on page
+            $('table').next().children().filter(function () {
+                var data = $(this);
+                console.log(data.children().first().next().text());
+//                out.push({
+//                    lp: data.children().first().text(),
+//                    name: data.children().first().next().text(),
+//                    href: data.children().first().children().first().attr('href'),
+//                    wazny: data.children().first().next().next().text(),
+//                    nieobecny: data.children().first().next().next().next().text()
+//                });
+            });
+            return resolve(out);
+        });
+    });
+}
+
+//votingSingleDetailsMarszalek(baseUrl + 'SQL2.nsf/InfoForPPIDL?OpenAgent&42885&PO')
+//        .then(function (data) {
+//            console.log(JSON.stringify(data, null, 2));
+//            console.log('--------------------------------------------');
+//        });
+
+function votingSingle(url) {
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, html) {
+            if (error) {
+                return reject(error);
+            }
+            var $ = cheerio.load(html);
+            var out = [];
+            $('table tbody tr').filter(function () {
+                var data = $(this);
+                out.push({
+                    klub: data.children().first().text(),
+                    href: data.children().first().children().first().attr('href'),
+                    liczebnosc: data.children().first().next().text(),
+                    wazny: data.children().first().next().next().text(),
+                    nieobecny: data.children().first().next().next().next().text()
+                });
+            });
+            return resolve(out);
+        });
+    });
+}
+
+votingSingle(baseUrl + 'Sejm7.nsf/agent.xsp?symbol=glosowaniaL&NrKadencji=7&NrPosiedzenia=95&NrGlosowania=1')
+        .then(function (data) {
+            console.log(JSON.stringify(data, null, 2));
+            console.log('--------------------------------------------');
+        });
+
 
 
 function votingDay(url) {
@@ -29,64 +90,15 @@ function votingDay(url) {
     });
 }
 
-votingDay(baseUrl + 'agent.xsp?symbol=listaglos&IdDnia=1456')
-        .then(function (data) {
-            console.log(JSON.stringify(data, null, 2));
-            console.log('--------------------------------------------');
-        })
-        .catch(function (error) {
-            console.log('--------------------------------------------');
-            console.log('ERROR:');
-            console.log(error);
-            console.log('--------------------------------------------');
-        });
-
-
-
-
-
-
-
-
-
-
-//app.get('/', function (req, res) {
+//votingDay(baseUrl + 'Sejm7.nsf/agent.xsp?symbol=listaglos&IdDnia=1456')
+//        .then(function (data) {
+//            console.log(JSON.stringify(data, null, 2));
+//            console.log('--------------------------------------------');
+//        })
+//        .catch(function (error) {
+//            console.log('--------------------------------------------');
+//            console.log('ERROR:');
+//            console.log(error);
+//            console.log('--------------------------------------------');
+//        });
 //
-//    url = 'http://www.imdb.com/title/tt1229340/';
-//
-//    request(url, function (error, response, html) {
-//        if (!error) {
-//            var $ = cheerio.load(html);
-//
-//            var title, release, rating;
-//            var json = {title: "", release: "", rating: ""};
-//
-//            $('.header').filter(function () {
-//                var data = $(this);
-//                title = data.children().first().text();
-//                release = data.children().last().children().text();
-//
-//                json.title = title;
-//                json.release = release;
-//            });
-//
-//            $('.star-box-giga-star').filter(function () {
-//                var data = $(this);
-//                rating = data.text();
-//
-//                json.rating = rating;
-//            });
-//        }
-//
-//        res.send('OK');
-//    });
-//});
-//
-//var server = app.listen(80, function () {
-//
-//  var host = server.address().address;
-//  var port = server.address().port;
-//
-//  console.log('Example app listening at http://%s:%s', host, port);
-//
-//});
